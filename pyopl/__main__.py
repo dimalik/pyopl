268148e599da46bf262996327d3dbe260275124e
Dimitris Alikaniotis <dim.alikaniotis@gmail.com>
2015-08-20 01:00:12 +0100

    initial commit

---
+++ pyopl/__main__.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+#
+# Copyright (c) 2015, Dimitris Alikaniotis
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of this package nor the
+#   names of its contributors may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+# This software is provided by the copyright holders and contributors "as
+# is" and any express or implied warranties, including, but not limited
+# to, the implied warranties of merchantability and fitness for a
+# particular purpose are disclaimed. In no event shall Nathan Grigg be
+# liable for any direct, indirect, incidental, special, exemplary, or
+# consequential damages (including, but not limited to, procurement of
+# substitute goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether in
+# contract, strict liability, or tort (including negligence or otherwise)
+# arising in any way out of the use of this software, even if advised of
+# the possibility of such damage.
+#
+# (also known as the New BSD License)
+
+
+import sys
+import argparse
+
+from engines.crossref import CrossRefEngine as CrossRef
+from engines.arxiv import ArxivEngine as Arxiv
+
+AVAILABLE_ENGINES = (
+    CrossRef,
+    Arxiv
+)
+
+
+def main(args=None):
+    '''Called by commandline'''
+    if args is None:
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description="Python Online Paper Locator",
+                                     prog="pyopl")
+    parser.add_argument("-s", "--search", dest="search", action='store_true')
+    parser.add_argument("-f", "--fetch", dest="search", action='store_false')
+    parser.set_defaults(search=True)
+    parser.add_argument("-e", "--engines", type=str,
+                        help="Engines to query or get the citation from. If se\
+arching then multiple engines can be given, but only one when given the identi\
+fier.")
+    parser.add_argument("-b", "--bib", default='', type=str,
+                        help="The .bib file to save the citation to. If unspec\
+ified then the STDOUT is used.")
+    parser.add_argument("-n", "--secondary-bib", type=str, default=None,
+                        help="A bibliography directory. If this is specified a\
+ separate .bib file with a unique filename is going to be created at this loca\
+tion.")
+    parser.add_argument('-l', '--line-width', type=int,
+                        default=100, help='Lines longer than -l will be trimme\
+d to -l.')
+    parser.add_argument('key', type=str)
+
+    args = parser.parse_args(args)
+
+    if args.engines:
+        used_engines = [engine for engine in AVAILABLE_ENGINES
+                        if engine._get_name() in args.engines]
+    else:
+        used_engines = AVAILABLE_ENGINES
+
+    if args.search:
+        query_results = []
+        for engine in used_engines:
+            query_engine = engine(args.key)
+            query_results.extend(query_engine.get_items())
+
+        for query_item in query_results:
+            print query_item
+    else:
+        engine = used_engines[0](args.key)
+        engine.write_bib(bib_file=args.bib,
+                         secondary_bib=args.secondary_bib)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ setup.py
@@ -0,0 +1,51 @@
+"""\
+Provides a command line tool to fetch the citation for an academic paper
+in bibtex format.
+
+Usage:
+
+(to search)
+pyopl --search --engines=crossref "you talking to me caines"
+
+(to fetch)
+pyopl --fetch --engines=crossref 10.1515/9783110274059.177
+"""
+
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    sys.exit("""Error: Setuptools is required for installation.
+-> http://pypi.python.org/pypi/setuptools""")
+
+setup(
+    name="pyopl",
+    version="0.1",
+    description="Locates and fetches academic citations in bibtex format",
+    author="Dimitrios Alikaniotis",
+    author_email="da352@cam.ac.uk",
+    url="http://da352.user.srcf.net",
+    packages=find_packages(),
+    keywords=["arxiv", "bibtex", "latex", "citation"],
+    entry_points={
+        'console_scripts': ['pyopl = pyopl.__main__:main']    
+    },
+    license="BSD",
+    install_requires=[
+        'arxiv2bib',
+        'feedparser'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing :: Markup :: LaTeX",
+        "Environment :: Console"
+    ],
+    long_description=__doc__,    
+)
